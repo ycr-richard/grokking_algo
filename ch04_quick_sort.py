504f8f8a2f37eef70f1e67a2bd8749498ae30693
ycr-richard <163023856+ycr-richard@users.noreply.github.com>
2026-06-09 10:19:52 +0800

    feat: add countdown (recursion) and quicksort

---
+++ ch04_quick_sort.py
@@ -0,0 +1,11 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+print(quicksort([3, 1, 4, 15, 9, 2, 6]))
